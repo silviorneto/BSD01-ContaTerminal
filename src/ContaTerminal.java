464afcf382aee4fdfc1ab45e7bf06304a7da6ba4
Silvio Romano Neto <silvioromano.dev@gmail.com>
2023-08-21 01:04:48 -0300

    Initial Commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        System.out.println("### CADASTRO DE CONTA BANCÁRIA ###");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite o número da conta: ");
+        int accountNumber = sc.nextInt();
+
+        System.out.print("Digite o número da agência: ");
+        String accountAgency = sc.next();
+
+        sc.nextLine();
+        System.out.print("Digite o nome do cliente: ");
+        String clientName = sc.nextLine();
+
+        System.out.print("Digite o saldo da conta: ");
+        double balance = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", clientName, accountAgency, accountNumber, balance);
+    }
+}
